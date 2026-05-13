2366f92df73ab34ce1c0fc05822ebb0ceab3a520
Александр <aleksandr@MacBookPro.netis.cc>
2026-05-13 23:29:24 +0300

    deploy: fix admin nav links, tab from URL

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Service Worker for PWA
+const CACHE_NAME = "phone-repair-v1"
+const urlsToCache = [
+  "/",
+  "/account",
+  "/admin",
+]
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    })
+  )
+})
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    })
+  )
+})
